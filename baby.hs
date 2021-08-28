04312b1d08ae726137a8965194d66366730e110e
Mathias Helsen <helsen.mathias@gmail.com>
2021-08-28 15:09:31 +0200

    playing around

--- baby.hs
+++ baby.hs
@@ -1,6 +1,22 @@
 doubleMe x = x + x
 doubleUs x y = doubleMe x + doubleMe y
 
-doubleSmallerNumber x = if x > 100
+doubleSmallNumber x = if x > 100
     then x
     else 2*x
+
+doubleSmallNumber' x = (if x > 100 then x else 2*x) + 1
+
+boombangs xs = [if x > 10 then "boom" else "bang" | x <- xs, odd x]
+
+length' xs = sum [1 | _ <- xs]
+
+removeAllNonUpperCase :: [Char] -> [Char]
+removeAllNonUpperCase st = [c | c <- st, c `elem` ['A'..'Z']]
+
+factorial :: Integer -> Integer
+factorial x = product [1..x]
+
+factorial' x = 
+    if x == 0 then 1
+    else x*factorial'(x-1)
