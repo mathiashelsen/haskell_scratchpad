78fe6a0e1cb9938e8d29e63676e2d3a93fcc5fd5
Mathias Helsen <helsen.mathias@gmail.com>
2021-09-03 12:36:13 +0200

    added a basic quicksort

---
+++ quicksort.hs
@@ -0,0 +1,21 @@
+--quicksort :: (Ord a) => [a] -> [a]
+--quicksort (x:[]) = x
+--quicksort (x:y:[])
+--    | x > y = y:x:[]
+--    | otherwise = x:y:[]
+--quicksort (x:xs) = 
+
+splitlist :: (Ord a) => a -> [a] -> [a]
+splitlist x [] = []
+splitlist x [y] = [y]
+splitlist x (y:ys)
+    | y < x = y : splitlist x ys 
+    | otherwise = splitlist x ys ++ [y]
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+    let smallerOrEqual = [y | y <- xs, y <= x]
+        larger = [y | y <- xs, y > x]
+    in quicksort smallerOrEqual ++ [x] ++ quicksort larger
+
