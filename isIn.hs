c46033ff29c81831ac7f33e728dc92bb0df09b54
Mathias Helsen <helsen.mathias@gmail.com>
2021-09-23 12:42:54 +0200

    added all functions required for isIn

--- isIn.hs
+++ isIn.hs
@@ -2,3 +2,12 @@ import Data.List
 
 isIn :: (Eq a) => [a] -> [a] -> Bool
 isIn xs = any (isInfixOf xs) . tails
+
+isInfixOf' :: (Eq a) => [a] -> [a] -> Bool
+isInfixOf'  _     []     = False
+isInfixOf' [x]    (y:ys) = x == y
+isInfixOf' (x:xs) (y:ys) = (x == y) && isInfixOf' xs ys
+
+tails' :: [a] -> [[a]]
+tails' [] = []
+tails' all@(x:xs) = all : tails' xs
